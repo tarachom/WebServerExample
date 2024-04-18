4958e6677237f355b76ac9ff126755cf2f014473
Yura <tarachom@gmail.com>
2024-04-18 11:13:57 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        var builder = WebApplication.CreateBuilder();
+        var app = builder.Build();
+
+        app.UseRouting();
+        app.MapGet("/", About);
+
+        app.Run();
+    }
+
+    static async Task About(HttpContext context)
+    {
+        HttpResponse response = context.Response;
+        await response.WriteAsync("Ok");
+    }
+}
